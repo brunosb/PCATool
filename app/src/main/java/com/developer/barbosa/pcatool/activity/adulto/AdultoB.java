a36eac7291b09cfb533672263ecfeab01f7a497b
Bruno Silva Barbosa <brunosb09celular@gmail.com>
2017-01-27 08:49:14 -0300

    Views Modulo Adulto

--- app/src/main/java/com/developer/barbosa/pcatool/activity/adulto/AdultoB.java
+++ app/src/main/java/com/developer/barbosa/pcatool/activity/adulto/AdultoB.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.developer.barbosa.pcatool.R;
 
@@ -14,6 +16,14 @@ public class AdultoB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adulto_b);
+
+        RadioGroup grupoQ1 = new RadioGroup(this);
+        RadioButton score4 = (RadioButton) findViewById(R.id.b14);
+        RadioButton score3 = (RadioButton) findViewById(R.id.b13);
+        RadioButton score2 = (RadioButton) findViewById(R.id.b12);
+        RadioButton score1 = (RadioButton) findViewById(R.id.b11);
+        RadioButton score9 = (RadioButton) findViewById(R.id.b19);
+
 
         final Button btn = (Button) findViewById(R.id.proximo);
         btn.setOnClickListener(new View.OnClickListener() {
